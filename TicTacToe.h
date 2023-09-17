0b8a6d6202776ed718e810ffbbab2cee72ad21e3
Erik Santana <eriksantana714@outlook.com>
2023-09-16 19:05:27 -0700

    new changes

--- TicTacToe.h
+++ TicTacToe.h
@@ -8,10 +8,33 @@ void updateBoard();
 
 const int ROW_SIZE = 3;
 const int COL_SIZE = 3;
+
+// move later???
 char boardUpdate[ROW_SIZE][COL_SIZE] = { {' ', ' ', ' '}, {' ', ' ', ' '}, {' ', ' ', ' '} };
 
 void ticTacToe()
 {
+
+	std::cout << "Tic-tac-toe (also known as Noughts and crosses or Xs and Os) is a game for two\n";
+	std::cout << "players, X and O, who take turns marking the spaces in a 3╫3 grid.The player who\n";
+	std::cout << "succeeds in placing three of their marks in a horizontal, vertical, or diagonal\n";
+	std::cout << "row wins the game.\n";
+	std::cout << "This tic-tac-toe program plays against the computer. Human player, X, will always\n";
+	std::cout << "first. Time will be recorded for the fastest and the slowest game. Average time will\n";
+	std::cout << "then be calculated and displayed.\n\n";
+	std::cout << "Game begins ...\n";
+
+	std::cout << "\n\n";
+	std::cout << "Tic - Tac - Toe\n";
+	std::cout << "|---|---|---|\n";
+	std::cout << "|   |   |   |\n";
+	std::cout << "|---|---|---|\n";
+	std::cout << "|   |   |   |\n";
+	std::cout << "|---|---|---|\n";
+	std::cout << "|   |   |   |\n";
+	std::cout << "|---|---|---|\n";
+	std::cout << "\n\n";
+
 	char currentPlayer = 'X'; // Start with player X
 
 	while (true) {
@@ -24,8 +47,10 @@ void ticTacToe()
 
 }
 
-
 void makeMove(char playerSymbol) {
+
+	std::cout << "Human moves ...\n\n";
+
 	// Take player input for row and column
 	int row = inputInteger("Enter the row (1..3): ", 1, 3) - 1; // Adjust input to 0-based index
 	int col = inputInteger("Enter the column (1..3): ", 1, 3) - 1;
