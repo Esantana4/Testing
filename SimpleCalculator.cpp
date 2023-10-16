d1e48483840f395ff0131fad576c5fc725f27fbc
Erik Santana <eriksantana714@outlook.com>
2023-10-16 11:59:16 -0700

    .h

--- SimpleCalculator.cpp
+++ SimpleCalculator.cpp
@@ -1,121 +1,294 @@
 #include "SimpleCalculator.h"
 
-// Implementation of a member function to determine the precedence of operators
-int SimpleCalculator::getOperatorPrecedence(char op) {
-    if (op == '^') return 3;
-    if (op == '*' || op == '/') return 2;
-    if (op == '+' || op == '-') return 1;
-    return 0; // Lower precedence for non-operators
+//precondition: going to call the ZeroDenominator class
+//postcondition: going to put it as a default constructor
+SimpleCalculator::DivisionByZero::DivisionByZero() {}
+//precondition: going to pass in the SimleCalculator class to give you access to the toString() function
+//postcondition: going to then now set the rational which is the ZeroDenominator private = to the toString()
+SimpleCalculator::DivisionByZero::DivisionByZero(SimpleCalculator r) {
+	checking = r.toString();
+}
+//precondition: going to call the toString() function
+//postcondition: going to return the rational (string)
+string SimpleCalculator::DivisionByZero::toString() {
+	return checking;
 }
 
-// Function to apply an operator to the operands on the stack
-void applyOperator(stack<double>& numbers, char op) {
+//Default Constructor
+//Precondition: NA
+//PostCondition: NA
+SimpleCalculator::SimpleCalculator() {}
 
-    // Check if there are at least two operands on the stack
-    if (numbers.size() < 2) { 
-        cerr << "Error: Insufficient operands for operator '" << op << "'\n"; // Print an error message if there are insufficient operands
-        return;
-    }
-   
-    double operand2 = numbers.top(); // Retrieve the top operand from the stack
-    numbers.pop(); // Remove the top operand from the stack
+//precondition: going to turn the variables to a string since they are int
+//postcondition: going to return a double checking if its 0
+string SimpleCalculator::toString() {
+	// Initialize a variable to hold the result (defaulted to 0.0).
+	double num2 = 0.0;
 
-    double operand1 = numbers.top(); // Retrieve the second top operand from the stack
-    numbers.pop(); // Remove the second top operand from the stack
-
-    switch (op) 
-    {
-    case '^':
-        numbers.push(pow(operand1, operand2)); // Push the result of raising operand1 to the power of operand2 onto the stack
-        break;
-    case '*':
-        numbers.push(operand1 * operand2); // Push the result of multiplying operand1 by operand2 onto the stack
-        break;
-    case '/':
-        // Check for division by zero before performing the division
-        if (operand2 != 0) {
-            numbers.push(operand1 / operand2); // Push the result of dividing operand1 by operand2 onto the stack
-        }
-        else {
-            cerr << "Error: Division by zero\n";
-            return;
-        }
-        break;
-    case '+':
-        numbers.push(operand1 + operand2); // Push the result of adding operand1 and operand2 onto the stack
-        break;
-    case '-':
-        numbers.push(operand1 - operand2); // Push the result of subtracting operand2 from operand1 onto the stack
-        break;
-    }
+	// Convert the result to a string and return it.
+	return to_string(num2);
 }
-
-// Member function to calculate the result of an expression
-double SimpleCalculator::calculateExpression(const string& expression) {
-
-    // Stacks to hold operands and operators during expression evaluation
-    stack<double> numbers;
-    stack<char> operators;
-
-    // Function pointer to get operator precedence
-    auto precedence = getOperatorPrecedence;
-
-    for (char ch : expression) {
-        // If the character is a digit, push its integer value onto the stack
-        if (isdigit(ch)) {
-            numbers.push(ch - '0'); // Convert character to integer
-        }
-        // If the character is an operator
-        else if (isOperator(ch)) {
-            // Process operators based on their precedence
-            while (!operators.empty() && precedence(operators.top()) >= precedence(ch)) {
-                applyOperator(numbers, operators.top());
-                operators.pop();
-            }
-            operators.push(ch); // Push the current operator onto the stack
-        }
-        // If the character is an opening parenthesis
-        else if (ch == '(') {
-            operators.push(ch); // Push '(' onto the stack
-        }
-        // If the character is a closing parenthesis
-        else if (ch == ')') {
-            // Process operators until the matching opening parenthesis is found
-            while (!operators.empty() && operators.top() != '(') {
-                applyOperator(numbers, operators.top());
-                operators.pop();
-            }
-            // If '(' is at the top, pop it to match the closing parenthesis
-            if (!operators.empty() && operators.top() == '(') {
-                operators.pop(); // Pop '('
-            }
-            // If there is no matching opening parenthesis, print an error
-            else {
-                cerr << "Error: Unmatched ')'\n";
-            }
-        }
-    }
-
-    while (!operators.empty()) {
-        applyOperator(numbers, operators.top());
-        operators.pop();
-    }
-
-    if (!numbers.empty()) {
-        return numbers.top();
-    }
-    else {
-        cerr << "Error: Empty result\n";
-        return 0.0;
-    }
-}
-
+//Precondition: NA
+//Postcondition: Displays Calculator Menu
 void SimpleCalculator::calculatorMenu() {
-    string expression = inputString("Type a fully parenthesized arithmetic expression: ", true);
-    double result = calculateExpression(expression);
-    cout << "\nResult: " << result << endl;
+
+	// Create stacks to store numbers and operators.
+	stack<double> numbers;
+	double answer;
+
+	// Clear the console screen.
+	system("cls");
+	// Display menu information.
+	cout << "\n\t1> Simple Calculator\n";
+	cout << string(100, char(196)) << endl;
+
+	try {
+		cout << "Type a parenthesized or non-parenthesized arithmetic expression with positive integers or doubles:\n\n";
+
+		// Clear the input buffer.
+		cin.clear();
+		// Specifies the maximum number of characters to ignore. It  provides the maximum value representable by the streamsize type.
+		cin.ignore(numeric_limits<streamsize>::max(), '\n'); // '\n': This is the delimiter.The ignore function will stop ignoring characters once it encounters the specified delimiter
+
+		// Call the displayCalculation function to process the input expression.
+		displayCalculation(numbers, cin, answer);
+	}
+	// Catch the DivisionByZero exception.
+	catch (DivisionByZero) {
+		cout << "\n\tERROR: Division by zero cannot be done.\n";
+	}
 }
 
-bool SimpleCalculator::isOperator(char ch) {
-    return ch == '^' || ch == '*' || ch == '/' || ch == '+' || ch == '-';
+//Precondition: A stack of doubles, user input with numbers that are positive, a double answer
+//Postcondition: Displays expression result
+void SimpleCalculator::displayCalculation(stack<double>& numbers, istream& ins, double& answer) {
+	const char RIGHT_PARENTHESIS = ')';
+	const char LEFT_PARENTHESIS = '(';
+	const char DECIMAL = '.';
+	double number;
+	char operation;
+	char parenthesis;
+	stack<char> convert;
+	stack<double> storage;
+	ofstream outFile;
+	int errorCtr = 0;
+	int specialError = 0;
+
+	// Open an output file stream to write to "Store.txt".
+	outFile.open("Store.txt");
+
+	// Loop through the input stream until a newline character is encountered.
+	while (ins && ins.peek() != '\n') {
+		// Check if the next character is a left parenthesis.
+		if (ins.peek() == LEFT_PARENTHESIS) {
+			ins >> parenthesis;
+			// Push the left parenthesis onto the 'convert' stack.
+			convert.push(parenthesis);
+		}
+		// Check if the next character is a space.
+		else if (ins.peek() == ' ') {
+			ins.ignore();
+		}
+		// Check if the next character is a digit or a decimal point.
+		else if (isdigit(ins.peek()) || ins.peek() == DECIMAL) {
+			ins >> number;
+			// Push the number onto the 'storage' stack.
+			storage.push(number);
+			// Write the number to the output file.
+			outFile << number << endl;
+		}
+		// Check if the next character is an arithmetic operation.
+		else if (ins.peek() == '+' || ins.peek() == '-' || ins.peek() == '*' || ins.peek() == '/' || ins.peek() == '^') {
+			ins >> operation;
+			// Pop operators from 'convert' stack and write to the output file based on precedence.
+			while (!convert.empty() && convert.top() != LEFT_PARENTHESIS && getPrecendence(convert.top()) >= getPrecendence(operation)) {
+				outFile << convert.top() << endl;
+				convert.pop();
+			}
+			// Push the current operation onto the 'convert' stack.
+			convert.push(operation);
+		}
+		// Check if the next character is a right parenthesis.
+		else if (ins.peek() == RIGHT_PARENTHESIS && !convert.empty()) {
+			ins.ignore();
+			// Pop operators from 'convert' stack and write to the output file until a left parenthesis is encountered.
+			while (!convert.empty() && convert.top() != LEFT_PARENTHESIS) {
+				operation = convert.top();
+				outFile << operation << endl;
+				convert.pop();
+			}
+			// Check if there is a matching left parenthesis.
+			if (convert.empty()) {
+				cout << "ERROR: Unbalanced Parenthesis\n";
+				++errorCtr;
+			}
+			else {
+				// Pop the left parenthesis.
+				convert.pop();
+			}
+		}
+		// Handle other cases (invalid characters).
+		else {
+			cout << "ERROR: Incorrect Expression Input\n";
+			++errorCtr;
+			++specialError;
+			// Ignore the rest of the line.
+			while (ins.peek() != '\n') {
+				ins.ignore();
+			}
+			break;
+		}
+	}
+
+	// Pop any remaining operators from 'convert' stack and write to the output file.
+	while (!convert.empty() && convert.top() != LEFT_PARENTHESIS && specialError == 0) {
+		operation = convert.top();
+		outFile << operation << endl;
+		convert.pop();
+	}
+
+	// Check for errors related to unbalanced parentheses.
+	if (!convert.empty() && specialError == 0) {
+		cout << "ERROR: Unbalanced Parenthesis\n";
+		++errorCtr;
+	}
+
+	// Check for errors related to incorrect expression input.
+	if (storage.size() <= 1 && specialError == 0) {
+		cout << "ERROR: Incorrect Expression Input\n";
+		++errorCtr;
+	}
+
+	// If no errors occurred, evaluate the expression and display the result.
+	if (errorCtr >= 1) {
+		// Do nothing if there are errors.
+	}
+	else {
+		// Evaluate the expression.
+		evaluateExpression(numbers);
+
+		// Get the result from the top of the 'numbers' stack.
+		answer = numbers.top();
+
+		// Display the result of the expression.
+		cout << "\nThe Expression Evaluates to: " << answer << endl;
+	}
+
+	// Close the output file.
+	outFile.close();
+}
+
+//Precondition:A stack of doubles for numbers
+//Postcondition: evaluates expression
+void SimpleCalculator::evaluateExpression(stack<double>& numbers) {
+	// Declare variables to store numbers, answer, and operators.
+	double num1;
+	double num2;
+	double storeNum;
+	char storeOp;
+
+	// Create an input file stream to read from "Store.txt".
+	ifstream inFile;
+	inFile.open("Store.txt");
+
+	// Continue reading until the end of the file (EOF) is reached.
+	while (!inFile.eof()) {
+		// Check if the next character in the file is a digit.
+		if (isdigit(inFile.peek())) {
+			inFile >> storeNum;
+			// Push the number onto the 'numbers' stack.
+			numbers.push(storeNum);
+		}
+		// Check if the next character in the file is a newline character.
+		else if (inFile.peek() == '\n') {
+			inFile.ignore();
+		}
+		// Check if the next character in the file is an addition operator ('+').
+		else if (inFile.peek() == '+' && numbers.size() >= 2) {
+			inFile >> storeOp;
+			// Pop the top two numbers from the 'numbers' stack.
+			num2 = numbers.top();
+			numbers.pop();
+			num1 = numbers.top();
+			numbers.pop();
+			// Calculate the sum and push it onto the stack.
+			numbers.push(num1 + num2);
+		}
+		// Check if the next character in the file is a subtraction operator ('-').
+		else if (inFile.peek() == '-' && numbers.size() >= 2) {
+			inFile >> storeOp;
+			num2 = numbers.top();
+			numbers.pop();
+			num1 = numbers.top();
+			numbers.pop();
+			// Calculate the difference and push it onto the stack.
+			numbers.push(num1 - num2);
+		}
+		// Check if the next character in the file is a multiplication operator ('*').
+		else if (inFile.peek() == '*' && numbers.size() >= 2) {
+			inFile >> storeOp;
+			num2 = numbers.top();
+			numbers.pop();
+			num1 = numbers.top();
+			numbers.pop();
+			// Calculate the product and push it onto the stack.
+			numbers.push(num1 * num2);
+		}
+		// Check if the next character in the file is a division operator ('/').
+		else if (inFile.peek() == '/' && numbers.size() >= 2) {
+			inFile >> storeOp;
+			num2 = numbers.top();
+			numbers.pop();
+			num1 = numbers.top();
+			numbers.pop();
+			// Check if division by zero is attempted.
+			if (num2 != 0) {
+				// Calculate the division and push it onto the stack.
+				numbers.push(num1 / num2);
+			}
+			// If division by zero is attempted, throw a DivisionByZero exception.
+			else {
+				throw DivisionByZero(*this);
+			}
+		}
+		// Check if the next character in the file is an exponentiation operator ('^').
+		else if (inFile.peek() == '^' && numbers.size() >= 2) {
+			inFile >> storeOp;
+			num2 = numbers.top();
+			numbers.pop();
+			num1 = numbers.top();
+			numbers.pop();
+			// Calculate the result of exponentiation and push it onto the stack.
+			numbers.push(pow(num1, num2));
+		}
+		// Check if there are less than two numbers on the stack while the file is not at the end.
+		else if (numbers.size() < 2 && !inFile.eof()) {
+			// Print an error message indicating incorrect input.
+			cout << "ERROR: Incorrect input must be positive numbers\n\n";
+			// Return early from the function.
+			return;
+		}
+	}
+
+	// Close the input file.
+	inFile.close();
+}
+
+//Precondition: a char operator
+//Postcondition: returns precendence value;
+int SimpleCalculator::getPrecendence(char operation) {
+	// Switch statement to determine the precedence of the given operation.
+	switch (operation) {
+	case '+': // Addition and subtraction have the same precedence (1).
+	case '-':
+		return 1;
+	case '*': // Multiplication and division have the same precedence (2).
+	case '/':
+		return 2;
+	case '^': // Exponentiation has the highest precedence (3).
+		return 3;
+	default:
+		// Handle any other characters.
+		return 0;
+	}
 }
--- SimpleCalculator.h
+++ SimpleCalculator.h
@@ -1,22 +1,33 @@
 #pragma once
 #include<iostream>
-#include <stack>
-#include <cmath> // for pow
-#include <cctype> // for isDigit
+#include<stack>
+#include<string>
+#include<fstream>
 using namespace std;
 
+// Used to input the mathematical expression inside the calculatorMenu()
 string inputString(string prompt, bool spaces);
-char inputChar(string prompt, string options);
-void mainMenu();
 
-class SimpleCalculator {
-private:
-    bool isOperator(char ch); // A private member function to check if a character is an operator.
-
+class SimpleCalculator
+{
 public:
-    double calculateExpression(const string& expression); // Function prototype for calculateExpression, calculates the result of an infix expression.
-    void calculatorMenu(); // Displays a menu for the calculator.
-    static int getOperatorPrecedence(char op); // Gets the precedence of an operator.
 
+	//Class that checks if its divison by 0 0
+	class DivisionByZero {
+	private:
+		string checking;
+	public:
+		DivisionByZero(); // Defualt Constructor
+		DivisionByZero(SimpleCalculator r); // Argument constructor - takes in a string that was converted from a double in toString().
+		string toString(); // returns the result of toString() from the SimpleCalculator class into the variable checking.
+	};
+
+	SimpleCalculator(); //defualt constructor 
+	string toString(); // Converts a variable to a string to easily check for zero in the DivisionByZero class by turning 0.0 into "0"
+	void calculatorMenu(); // Stores numbers and operators into stacks and displayCalculation(numbers, cin, answer); does the process.
+	void displayCalculation(stack<double>& numbers, istream& ins, double& answer); // Checks for parentheses while also pushes & pops numbers and operators until an answer can be evaluated here: evaluateExpression(numbers)
+	void evaluateExpression(stack<double>& numbers); // Evaluates and calculates the numbers through mathematical operations(^,*,/,+,-). Then pushes the answer to stack<double>numbers 
+	int getPrecendence(char operation); // Returns the precedence of the operations. (lowest to Highest) 1. +, -  2. *, / 3. ^
 };
+
 
